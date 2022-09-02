4ffe7b4d0970f3c6c790f1626c16f6b965364101
cristiannd <cristian.donalicio@gmail.com>
2022-09-02 17:43:45 -0300

    Added mongodb with mongoose, env and new frontend

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
-  console.log('-------LOGGER--------')
+  console.log('--------LOGGER--------')
   console.log('Method:', request.method)
   console.log('Path:', request.path)
   console.log('Body:', request.body)
-  console.log('-------LOGGER--------')
+  console.log('--------LOGGER--------')
   next()
 }
 
@@ -20,57 +22,19 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+  const id = request.params.id
 
-  if (note) {
+  Note.findById(id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -81,21 +45,20 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId,
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
